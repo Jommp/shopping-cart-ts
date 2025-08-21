a3c8e50a68b4ccbddae05324602152b6f68a158b
Jommp <jommp.dev@gmail.com>
2025-08-21 14:12:01 -0500

    feat(types) create CartItem and CartProps types

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,3 +5,17 @@ export type Guitar = {
   description: string
   price: number
 }
+
+export type CartItem = Guitar & {
+  quantity: number
+}
+
+export type CartProps = {
+  cart: CartItem[]
+  removeFromCart: (itemId : CartItem['id']) => void
+  decreaseQuantity: (itemId : CartItem['id']) => void
+  increaseQuantity: (itemId : CartItem['id']) => void
+  clearCart: () => void
+  isEmptyCart: boolean
+  totalPrice: number
+}
